822f5d22bbc6094a359492b11d1e0ba1b9289da0
cryzed <cryzed@googlemail.com>
2014-09-09 17:27:48 +0200

    Clean up after toggling and restore original color scheme to the view

--- plugin.py
+++ plugin.py
@@ -12,7 +12,7 @@ APPDATA_PATH = None
 CACHE_PATH = None
 BLACK_RGB = (0, 0, 0)
 
-pulsing_views = set()
+pulsing_views = {}
 
 
 def plugin_loaded():
@@ -75,15 +75,23 @@ def pulse_view(view_id, changes, delay, pause):
 
         current_delay += delay
 
-    if view_id in pulsing_views:
+    if pulsing_views[view_id]['enabled']:
         sublime.set_timeout(lambda: pulse_view(view_id, changes, delay, pause), maximum_delay + pause)
+    else:
+        cleanup_pulsing_view(view_id)
+
+
+def cleanup_pulsing_view(view_id):
+    pulsing_views[view_id]['change_original_color_scheme']()
+    shutil.rmtree(get_cache_path(str(view_id)))
+    del pulsing_views[view_id]
 
 
 class TogglePulseViewEventListener(sublime_plugin.EventListener):
     def on_close(self, view):
         view_id = view.id()
         if view_id in pulsing_views:
-            pulsing_views.remove(view_id)
+            cleanup_pulsing_view(view_id)
 
 
 class TogglePulseViewCommand(sublime_plugin.TextCommand):
@@ -93,12 +101,13 @@ class TogglePulseViewCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, delta, delay, pause):
         view_id = self.view.id()
-        if view_id in pulsing_views:
-            pulsing_views.remove(view_id)
+        if view_id in pulsing_views and pulsing_views[view_id]['enabled']:
+            pulsing_views[view_id]['enabled'] = False
             return
 
         view_settings = self.view.settings()
-        property_list = plistlib.readPlistFromBytes(sublime.load_binary_resource(view_settings.get('color_scheme')))
+        original_color_scheme_settings_path = view_settings.get('color_scheme')
+        property_list = plistlib.readPlistFromBytes(sublime.load_binary_resource(original_color_scheme_settings_path))
 
         background_settings = []
         for setting in property_list['settings']:
@@ -107,12 +116,10 @@ class TogglePulseViewCommand(sublime_plugin.TextCommand):
                 background_settings.append(settings)
 
         view_cache_path = get_cache_path(str(view_id))
-        original_color_scheme_cache_path = os.path.join(view_cache_path, '0')
-        plistlib.writePlist(property_list, original_color_scheme_cache_path)
-        changes = [make_change_color_scheme_function(view_settings, make_settings_path(original_color_scheme_cache_path))]
-
+        change_original_color_scheme = make_change_color_scheme_function(view_settings, original_color_scheme_settings_path)
+        changes = [change_original_color_scheme]
         is_black = False
-        for difference in range(1, delta + 1):
+        for difference in range(delta):
             if is_black:
                 break
 
@@ -128,5 +135,5 @@ class TogglePulseViewCommand(sublime_plugin.TextCommand):
             plistlib.writePlist(property_list, color_scheme_cache_path)
             changes.append(make_change_color_scheme_function(view_settings, make_settings_path(color_scheme_cache_path)))
 
-        pulsing_views.add(view_id)
+        pulsing_views[view_id] = {'enabled': True, 'change_original_color_scheme': change_original_color_scheme}
         pulse_view(view_id, changes, delay * 1000, pause * 1000)
